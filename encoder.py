8b9ef461c51c9592bf9ed71d886828ac4852dc3a
Anh Nguyễn Hoàng <anhnguyenhoang@Anhs-MacBook-Pro.local>
2024-05-25 11:38:13 +0700

    first commit

---
+++ encoder.py
@@ -0,0 +1,301 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from transformers import BertModel
+from transformers import RobertaModel
+import torch.nn.functional as F
+from transformers.models.llama.modeling_llama import *
+
+class EncodingModel(nn.Module):
+    def __init__(self, config):
+        nn.Module.__init__(self)
+        self.config = config
+        if config.model == 'bert':
+            self.encoder = BertModel.from_pretrained(config.bert_path).to(config.device)
+        elif config.model == 'roberta':
+            self.encoder = RobertaModel.from_pretrained(config.roberta_path).to(config.device)
+            self.encoder.resize_token_embeddings(config.vocab_size)
+        if config.tune == 'prompt':
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+
+        self.bert_word_embedding = self.encoder.get_input_embeddings()
+        self.embedding_dim = self.bert_word_embedding.embedding_dim
+        self.prompt_lens = config.prompt_len * config.prompt_num
+        self.softprompt_encoder = nn.Embedding(self.prompt_lens, self.embedding_dim).to(config.device)
+        # initialize prompt embedding
+        self._init_prompt()
+        self.prompt_ids = torch.LongTensor(list(range(self.prompt_lens))).to(self.config.device)
+
+    def _init_prompt(self):
+        # is is is [e1] is is is [MASK] is is is [e2] is is is
+        if self.config.prompt_init == 1:
+            prompt_embedding = torch.zeros_like(self.softprompt_encoder.weight).to(self.config.device)
+            token_embedding = self.bert_word_embedding.weight[2003]
+            prompt_embedding[list(range(self.prompt_lens)), :] = token_embedding.clone().detach()
+            for param in self.softprompt_encoder.parameters():
+                param.data = prompt_embedding # param.data
+       
+        # ! @ # [e1] he is as [MASK] * & % [e2] just do it  
+        elif self.config.prompt_init == 2:
+            prompt_embedding = torch.zeros_like(self.softprompt_encoder.weight).to(self.config.device)
+            ids = [999, 1030, 1001, 2002, 2003, 2004, 1008, 1004, 1003, 2074, 2079,  2009]
+            for i in range(self.prompt_lens):
+                token_embedding = self.bert_word_embedding.weight[ids[i]]
+                prompt_embedding[i, :] = token_embedding.clone().detach()
+            for param in self.softprompt_encoder.parameters():
+                param.data = prompt_embedding # param.data
+
+
+    def embedding_input(self, input_ids): # (b, max_len)
+        input_embedding = self.bert_word_embedding(input_ids) # (b, max_len, h)
+        prompt_embedding = self.softprompt_encoder(self.prompt_ids) # (prompt_len, h)
+
+        for i in range(input_ids.size()[0]):
+            p = 0
+            for j in range(input_ids.size()[1]):
+                if input_ids[i][j] == self.config.prompt_token_ids:
+                    input_embedding[i][j] = prompt_embedding[p]
+                    p += 1
+
+        return input_embedding
+
+
+    def forward(self, inputs): # (b, max_length)
+        batch_size = inputs['ids'].size()[0]
+        tensor_range = torch.arange(batch_size) # (b)     
+        pattern = self.config.pattern
+        if pattern == 'softprompt' or pattern == 'hybridprompt':
+            input_embedding = self.embedding_input(inputs['ids'])
+            outputs_words = self.encoder(inputs_embeds=input_embedding, attention_mask=inputs['mask'])[0]
+        else:
+            outputs_words = self.encoder(inputs['ids'], attention_mask=inputs['mask'])[0] # (b, max_length, h)
+
+        # return [CLS] hidden
+        if pattern == 'cls' or pattern == 'softprompt':
+            clss = torch.zeros(batch_size, dtype=torch.long)
+            return outputs_words[tensor_range ,clss] # (b, h)
+
+        # return [MASK] hidden
+        elif pattern == 'hardprompt' or pattern == 'hybridprompt':
+            masks = []
+            for i in range(batch_size):
+                ids = inputs['ids'][i].cpu().numpy()
+                mask = np.argwhere(ids == self.config.mask_token_ids)[0][0]
+                masks.append(mask)
+            mask_hidden = outputs_words[tensor_range, torch.tensor(masks)] # (b, h)
+            return mask_hidden
+
+        # return e1:e2 hidden
+        elif pattern == 'marker':
+            h1, t1 = [], []
+            for i in range(batch_size):
+                ids = inputs['ids'][i].cpu().numpy()
+                h1_index, t1_index = np.argwhere(ids == self.config.h_ids), np.argwhere(ids == self.config.t_ids)
+                h1.append(0) if h1_index.size == 0 else h1.append(h1_index[0][0])
+                t1.append(0) if t1_index.size == 0 else t1.append(t1_index[0][0])
+
+            h_state = outputs_words[tensor_range, torch.tensor(h1)] # (b, h)
+            t_state = outputs_words[tensor_range, torch.tensor(t1)]
+
+            concerate_h_t = (h_state + t_state) / 2 # (b, h)
+            return concerate_h_t
+
+class LlamaClassification(LlamaPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        # self.num_labels = config.num_labels
+        self.model = LlamaModel(config)
+        # self.ln = nn.Linear(config.hidden_size, self.config.hidden_size, bias=True)
+        # self.dropout = nn.Dropout(0.1)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def set_input_embeddings(self, value):
+        self.model.embed_tokens = value
+
+    @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        get_feature: Optional[bool] = False,
+    ) -> Union[Tuple, SequenceClassifierOutputWithPast]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        transformer_outputs = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        hidden_states = transformer_outputs[0]
+        
+
+        e11 = []
+        # for each sample in the batch, acquire the positions of its [E11] and [E21]
+        for i in range(input_ids.shape[0]):
+            tokens = input_ids[i].cpu().numpy()
+            try:
+                e11.append(np.argwhere(tokens == 2)[0][0] - 1)
+            except:
+                e11.append(len(tokens) - 1)
+        
+        output = []
+        for i in range(len(e11)):
+            instance_output = torch.index_select(hidden_states, 0, torch.tensor(i).to(hidden_states.device))
+            instance_output = torch.index_select(instance_output, 1, torch.tensor([e11[i]]).to(hidden_states.device))
+            output.append(instance_output)  # [B,N,H] --> [B,1,H]
+        
+        output = torch.stack(output)
+        output = output.view(output.shape[0],-1) # [B,1,H] --> [B,H]
+        return output
+    
+class LlamaLMClassification(LlamaPreTrainedModel):
+    _tied_weights_keys = ["lm_head.weight"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.model = LlamaModel(config)
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.info_nce_fc = nn.Linear(config.vocab_size, config.hidden_size , bias= False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def infoNCE_f(self,V,C):
+        """
+        V : 1 x dim_V
+        C : 1 x dim_C
+
+        """
+        out = self.info_nce_fc(V) # N x dim_C
+        C = C.to(out.device)
+        out = torch.matmul(out, C.t()) # N x N
+        return out
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def set_input_embeddings(self, value):
+        self.model.embed_tokens = value
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head = new_embeddings
+
+    def set_decoder(self, decoder):
+        self.model = decoder
+
+    def get_decoder(self):
+        return self.model
+
+    @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        r"""
+        Args:
+            labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        Returns:
+
+        Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, LlamaForCausalLM
+
+        >>> model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+        >>> tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+        >>> prompt = "Hey, are you conscious? Can you talk to me?"
+        >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+        >>> # Generate
+        >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+        >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+        ```"""
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cache_position=cache_position,
+        )
+
+        hidden_states = outputs[0] #B, N, H
+
+        e11 = []
+        # for each sample in the batch, acquire the positions of its [E11] and [E21]
+        for i in range(input_ids.shape[0]):
+            tokens = input_ids[i].cpu().numpy()
+            try:
+                e11.append(np.argwhere(tokens == 2)[0][0] - 1)
+            except:
+                e11.append(len(tokens) - 1)
+        
+        output = []
+        # for each sample in the batch, acquire its representations for [E11] and [E21]
+        for i in range(len(e11)):
+            instance_output = torch.index_select(hidden_states, 0, torch.tensor(i).to(hidden_states.device))
+            instance_output = torch.index_select(instance_output, 1, torch.tensor([e11[i]]).to(hidden_states.device))
+            output.append(instance_output)  # [B,N,H] --> [B,1,H]
+        
+        output = torch.stack(output)
+        logit = self.lm_head(output) # B,1,V
+        output = output.view(output.shape[0],-1) # [B,1,H] --> [B,H]
+
+        return output, logit.squeeze(1)
